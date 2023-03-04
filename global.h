12f338fe8e5316ad60c87e2698f9ebd12cb49927
nhphucqt <hoangphuc.qti@gmail.com>
2023-03-04 19:32:00 +0700

    Move constants to global.h

--- global.h
+++ global.h
@@ -10,4 +10,18 @@
 namespace raylib {
     #include "raylib.h"
 }
+
+namespace Animate {
+    const double FADEIN_SPEED = 512;
+    const double FADEOUT_SPEED = 512;
+    const double TRANS_TIME = 3;
+}
+
+namespace Core {
+    const int FPS = 360;
+    // WXGA+ | 16 : 10
+    const int WINDOW_WIDTH = 1440;
+    const int WINDOW_HEIGHT = 900;
+}
+
 #endif
--- main.cpp
+++ main.cpp
@@ -4,14 +4,6 @@ raylib::Vector2 operator - (const raylib::Vector2 &a, const raylib::Vector2 &b) 
     return raylib::Vector2{a.x-b.x, a.y-b.y};
 }
 
-// WXGA | 16 : 10
-const int WINDOW_WIDTH = 1280;
-const int WINDOW_HEIGHT = 800;
-
-const double ANIMATE_FADEIN_SPEED = 512;
-const double ANIMATE_FADEOUT_SPEED = 512;
-const double ANIMATE_TRANS_TIME = 1.5;
-
 struct Node {
 	double x, y;
     double lx, ly;
@@ -37,7 +29,7 @@ struct Node {
 };
 
 bool nodeFadeIn(Node* pNode, double* elapseTime) {
-    double inc = ANIMATE_FADEIN_SPEED * *elapseTime;
+    double inc = Animate::FADEIN_SPEED * *elapseTime;
     if ((pNode->transparent+=inc) >= pNode->trueColor.a) {
         pNode->currColor.a = pNode->trueColor.a;
         return true;
@@ -48,7 +40,7 @@ bool nodeFadeIn(Node* pNode, double* elapseTime) {
 }
 
 bool nodeFadeOut(Node* pNode, double* elapseTime) {
-    double dec = ANIMATE_FADEOUT_SPEED * *elapseTime;
+    double dec = Animate::FADEOUT_SPEED * *elapseTime;
     if ((pNode->transparent-=dec) <= 0) {
         pNode->currColor.a = 0;
         return true;
@@ -62,50 +54,28 @@ float getDist(float x, float y, float u, float v) {
     return sqrt((x-u)*(x-u) + (y-v)*(y-v));
 }
 
-std::pair<double,double> unitVector(double x, double y, double u, double v) {
-    double Vx = u - x;
-    double Vy = v - y; 
-    double len = sqrt(Vx*Vx+Vy*Vy);
-    return std::make_pair(Vx / len, Vy / len);
-}
-
 double bezier(double S, double T, double t) {
     // std::pair<double,double> A(0,0);
-    // std::pair<double,double> B(T/2,0);
-    // std::pair<double,double> C(0,S);
+    // std::pair<double,double> B(T+500000,-500);
+    // std::pair<double,double> C(0-500000,S+500);
     // std::pair<double,double> D(T,S);
+
+    // std::pair<double,double> A(0,0);
+    // std::pair<double,double> B(T+500000,-500);
+    // std::pair<double,double> C(0-500000,S+500);
+    // std::pair<double,double> D(T,S);
+
     std::pair<double,double> A(0,0);
     std::pair<double,double> B(T,0);
     std::pair<double,double> C(0,S);
     std::pair<double,double> D(T,S);
     // A -> B -> C -> D
     double p = t / T;
-
-    // double x_a = A.x + (B.x - A.x) * p;
-    // double y_a = A.y + (B.y - A.y) * p;
-    // double x_b = B.x + (C.x - B.x) * p;
-    // double y_b = B.y + (C.y - B.y) * p;
-    // double x_c = C.x + (D.x - C.x) * p;
-    // double y_c = C.y + (D.y - C.y) * p;
-
-    // double x_d = x_a + (x_b - x_a) * p;
-    // double y_d = y_a + (y_b - y_a) * p;
-    // double x_e = x_b + (x_c - x_b) * p;
-    // double y_e = y_b + (y_c - y_b) * p;
-
-    // double x_g = x_d + (x_e - x_d) * p;
-    // double y_g = y_d + (y_e - y_d) * p;
-
     double tA = -p*p*p + 3*p*p - 3*p + 1;
     double tB = 3*p*p*p - 6*p*p + 3*p;
     double tC = -3*p*p*p + 3*p*p;
     double tD = p*p*p;
-
-    // double rX = A.first * tA + B.first * tB + C.first * tC + D.first * tD;
     double rY = A.second * tA + B.second * tB + C.second * tC + D.second * tD;
-
-    std::cerr << " >>+ " << S << ' ' << p << ' ' << rY << '\n';
-
     return rY;
 }
 
@@ -144,12 +114,12 @@ void runScenes(std::queue<std::vector<std::function<bool()>>> &queueOfScenes, do
 }
 
 int main() {
-    raylib::InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "demo");
-    raylib::SetTargetFPS(360);
+    raylib::InitWindow(Core::WINDOW_WIDTH, Core::WINDOW_HEIGHT, "demo");
+    raylib::SetTargetFPS(Core::FPS);
 
     const int NODE_SIZE = 50;
-    Node* node = new Node(100, 100, NODE_SIZE, CLITERAL(raylib::Color){230, 41, 55, 0}, CLITERAL(raylib::Color){230, 41, 55, 255}, true);
-    Node* node_2 = new Node(0, 0, NODE_SIZE, CLITERAL(raylib::Color){0, 228, 48, 0}, CLITERAL(raylib::Color){0, 228, 48, 255}, true);
+    Node* node = new Node(10, 10, NODE_SIZE, CLITERAL(raylib::Color){230, 41, 55, 0}, CLITERAL(raylib::Color){230, 41, 55, 160}, true);
+    Node* node_2 = new Node(0, 0, NODE_SIZE, CLITERAL(raylib::Color){0, 228, 48, 0}, CLITERAL(raylib::Color){0, 228, 48, 160}, true);
 
     std::queue<std::vector<std::function<bool()>>> queueOfScenes;
 
@@ -162,8 +132,8 @@ int main() {
     });
 
     queueOfScenes.push({
-        std::bind(&nodeTransform, node, WINDOW_WIDTH-NODE_SIZE, WINDOW_HEIGHT-NODE_SIZE, elapseTime, currTime, ANIMATE_TRANS_TIME),
-        std::bind(&nodeTransform, node_2, WINDOW_WIDTH-NODE_SIZE, WINDOW_HEIGHT-NODE_SIZE*2, elapseTime, currTime, ANIMATE_TRANS_TIME)
+        std::bind(&nodeTransform, node, Core::WINDOW_WIDTH-NODE_SIZE, Core::WINDOW_HEIGHT-NODE_SIZE, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&nodeTransform, node_2, Core::WINDOW_WIDTH-NODE_SIZE, Core::WINDOW_HEIGHT-NODE_SIZE*2, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
         std::bind(&nodeFadeOut, node, elapseTime)
@@ -172,31 +142,31 @@ int main() {
         std::bind(&nodeFadeIn, node, elapseTime)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform, node, 300, 400, elapseTime, currTime, ANIMATE_TRANS_TIME),
-        std::bind(&nodeTransform, node_2, 450, 450, elapseTime, currTime, ANIMATE_TRANS_TIME)
+        std::bind(&nodeTransform, node, 300, 400, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&nodeTransform, node_2, 450, 450, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform, node, 300, 300, elapseTime, currTime, ANIMATE_TRANS_TIME),
-        std::bind(&nodeTransform, node_2, 0, 450, elapseTime, currTime, ANIMATE_TRANS_TIME)
+        std::bind(&nodeTransform, node, 300, 300, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&nodeTransform, node_2, 0, 450, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform, node, 0, 0, elapseTime, currTime, ANIMATE_TRANS_TIME)
+        std::bind(&nodeTransform, node, 0, 0, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform, node, 0, 450, elapseTime, currTime, ANIMATE_TRANS_TIME),
-        std::bind(&nodeTransform, node_2, 450, 0, elapseTime, currTime, ANIMATE_TRANS_TIME)
+        std::bind(&nodeTransform, node, 0, 450, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&nodeTransform, node_2, 450, 0, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform, node, 450, 450, elapseTime, currTime, ANIMATE_TRANS_TIME),
-        std::bind(&nodeTransform, node_2, 0, 0, elapseTime, currTime, ANIMATE_TRANS_TIME)
+        std::bind(&nodeTransform, node, 450, 450, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&nodeTransform, node_2, 0, 0, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform, node, 450, 0, elapseTime, currTime, ANIMATE_TRANS_TIME),
-        std::bind(&nodeTransform, node_2, 0, 450, elapseTime, currTime, ANIMATE_TRANS_TIME)
+        std::bind(&nodeTransform, node, 450, 0, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&nodeTransform, node_2, 0, 450, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform, node, 0, 0, elapseTime, currTime, ANIMATE_TRANS_TIME),
-        std::bind(&nodeTransform, node_2, 450, 450, elapseTime, currTime, ANIMATE_TRANS_TIME)
+        std::bind(&nodeTransform, node, 0, 0, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&nodeTransform, node_2, 450, 450, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
         std::bind(&nodeFadeOut, node, elapseTime),
