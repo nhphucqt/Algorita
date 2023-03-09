f3c3de0e56dc6a60a6fa0ff25afafd94b7e85d11
nhphucqt <hoangphuc.qti@gmail.com>
2023-03-09 08:06:48 +0700

    Edit constructor

--- libs/core/SinglyNode.cpp
+++ libs/core/SinglyNode.cpp
@@ -1,7 +1,9 @@
 #include "SinglyNode.h"
 
-SinglyNode::SinglyNode() : GraphicNode::GraphicNode() {}
+SinglyNode::SinglyNode() : GraphicNode::GraphicNode() {
+    pNext = nullptr;
+}
 
-SinglyNode::SinglyNode(double _x, double _y, int _s, bool _sqr, int _v) : GraphicNode::GraphicNode(_x, _y, _s, _sqr) {
-    val = _v;
+SinglyNode::SinglyNode(double _x, double _y, int _s, bool _sqr, int _v) : GraphicNode::GraphicNode(_x, _y, _s, _sqr, _v) {
+    pNext = nullptr;
 }  
--- libs/core/SinglyNode.h
+++ libs/core/SinglyNode.h
@@ -1,11 +1,13 @@
 #ifndef SINGLY_NODE_H
 #define SINGLY_NODE_H
 
+#include <string>
+#include "../global.h"
 #include "../graphic/GraphicNode.h"
 
 class SinglyNode : public GraphicNode {
-    int val;
-    SinglyNode *pNext, *pPrev;
+public:
+    SinglyNode *pNext;
     SinglyNode();
     SinglyNode(double _x, double _y, int _s, bool _sqr, int _v);
 };
