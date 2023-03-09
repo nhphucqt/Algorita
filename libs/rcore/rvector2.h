34db9db7d9dc29f10fd6571bfc717a018f743d8b
nhphucqt <hoangphuc.qti@gmail.com>
2023-03-09 08:06:31 +0700

    Add templated function toVector

--- libs/rcore/rvector2.h
+++ libs/rcore/rvector2.h
@@ -6,6 +6,11 @@ namespace raylib {
     #include "raylib.h"
 }
 
+template<typename T>
+raylib::Vector2 toVector(const T &x, const T &y) {
+    return raylib::Vector2{(float)(x),(float)(y)};
+}
+
 raylib::Vector2 operator - (const raylib::Vector2 &a, const raylib::Vector2 &b);
 float getDist(const raylib::Vector2 &v1, const raylib::Vector2 &v2);
 
