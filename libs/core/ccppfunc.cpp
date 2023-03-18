57ddb5d035d67692fdcbe71cd9c2b92a82e02bbc
nhphucqt <hoangphuc.qti@gmail.com>
2023-03-18 17:33:25 +0700

    Change file name

---
+++ libs/core/ccppfunc.cpp
@@ -0,0 +1,15 @@
+#include "ccppfunc.h"
+
+std::string cf::num2str(int x) {
+    if (x == 0) {
+        return "0";
+    } else {
+        std::string ans;
+        while (x > 0) {
+            ans += x % 10 +  '0';
+            x /= 10;
+        }
+        std::reverse(ans.begin(),ans.end());
+        return ans;
+    }
+}
---
+++ libs/core/ccppfunc.h
@@ -0,0 +1,11 @@
+#ifndef ccppfuncUNC_H
+#define ccppfuncUNC_H
+
+#include <string>
+#include <algorithm>
+
+namespace cf {
+    std::string num2str(int x);
+}
+
+#endif
