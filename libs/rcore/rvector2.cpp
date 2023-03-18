907c5a2f9814ce59227b787bb5ade47cc5feac14
nhphucqt <hoangphuc.qti@gmail.com>
2023-03-18 17:42:48 +0700

    Add features

--- libs/rcore/rvector2.cpp
+++ libs/rcore/rvector2.cpp
@@ -4,6 +4,17 @@ raylib::Vector2 operator - (const raylib::Vector2 &a, const raylib::Vector2 &b) 
     return raylib::Vector2{a.x-b.x, a.y-b.y};
 }
 
+raylib::Vector2 operator + (const raylib::Vector2 &a, const raylib::Vector2 &b) {
+    return raylib::Vector2{a.x+b.x, a.y+b.y};
+}
+
+raylib::Vector2 operator - (const TrVector &tv, const raylib::Vector2 &v) {
+    return raylib::Vector2{(float)(*tv.x - v.x), (float)(*tv.y - v.y)};
+}
+raylib::Vector2 operator + (const TrVector &tv, const raylib::Vector2 &v) {
+    return raylib::Vector2{(float)(*tv.x + v.x), (float)(*tv.y + v.y)};
+}
+
 float getDist(const raylib::Vector2 &v1, const raylib::Vector2 &v2) {
     return sqrt((v1.x-v2.x)*(v1.x-v2.x) + (v1.y-v2.y)*(v1.y-v2.y));
 }
--- libs/rcore/rvector2.h
+++ libs/rcore/rvector2.h
@@ -5,13 +5,19 @@
 namespace raylib {
     #include "raylib.h"
 }
+#include "../core/ccppclass.h"
 
 template<typename T>
-raylib::Vector2 toVector(const T &x, const T &y) {
+raylib::Vector2 toVector2(const T &x, const T &y) {
     return raylib::Vector2{(float)(x),(float)(y)};
 }
 
 raylib::Vector2 operator - (const raylib::Vector2 &a, const raylib::Vector2 &b);
+raylib::Vector2 operator + (const raylib::Vector2 &a, const raylib::Vector2 &b);
+
+raylib::Vector2 operator - (const TrVector &tv, const raylib::Vector2 &v);
+raylib::Vector2 operator + (const TrVector &tv, const raylib::Vector2 &v);
+
 float getDist(const raylib::Vector2 &v1, const raylib::Vector2 &v2);
 
 #endif
