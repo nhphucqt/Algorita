269bd3b9eee756309f42cb3a21725b4212a78f1d
nhphucqt <hoangphuc.qti@gmail.com>
2023-03-22 00:29:23 +0700

    Rename function

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main() {
 
     const int NODE_SIZE = 50;
     SinglyNode* node = new SinglyNode(200, 200, NODE_SIZE, true, 12);
-    SinglyNode* node_2 = new SinglyNode(200-NODE_SIZE, 200, NODE_SIZE, false, 5);
+    SinglyNode* node_2 = new SinglyNode(200-NODE_SIZE, 200, NODE_SIZE, true, 5);
     node->setNext(node_2);
 
     std::queue<std::vector<std::function<bool()>>> queueOfScenes;
@@ -34,93 +34,93 @@ int main() {
     double* currTime = new double(0);
 
     queueOfScenes.push({
-        std::bind(&nodeFadeIn<GraphicNode>, node, elapseTime, currTime, Animate::FADEIN_TIME),
-        std::bind(&nodeFadeIn<GraphicNode>, node_2, elapseTime, currTime, Animate::FADEIN_TIME)
+        std::bind(&Animate::fadeIn<GraphicNode>, node, elapseTime, currTime, Animate::FADEIN_TIME),
+        std::bind(&Animate::fadeIn<GraphicNode>, node_2, elapseTime, currTime, Animate::FADEIN_TIME)
     });
 
     queueOfScenes.push({
-        std::bind(&nodeTransform<GraphicNode>, node, 500, 500, elapseTime, currTime, Animate::TRANS_TIME),
-        std::bind(&nodeTransform<GraphicNode>, node_2, 500, 500+NODE_SIZE, elapseTime, currTime, Animate::TRANS_TIME)
+        std::bind(&Animate::transform<GraphicNode>, node, 500, 500, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&Animate::transform<GraphicNode>, node_2, 500, 500+NODE_SIZE, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeFadeOut<GraphicNode>, node, elapseTime, currTime, Animate::FADEOUT_TIME)
+        std::bind(&Animate::fadeOut<GraphicNode>, node, elapseTime, currTime, Animate::FADEOUT_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeFadeIn<GraphicNode>, node, elapseTime, currTime, Animate::FADEIN_TIME)
+        std::bind(&Animate::fadeIn<GraphicNode>, node, elapseTime, currTime, Animate::FADEIN_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform<GraphicNode>, node, 300, 500, elapseTime, currTime, Animate::TRANS_TIME),
-        std::bind(&nodeTransform<GraphicNode>, node_2, 900, 700, elapseTime, currTime, Animate::TRANS_TIME)
+        std::bind(&Animate::transform<GraphicNode>, node, 300, 500, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&Animate::transform<GraphicNode>, node_2, 900, 700, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform<GraphicNode>, node, 1200, 300, elapseTime, currTime, Animate::TRANS_TIME),
-        std::bind(&nodeTransform<GraphicNode>, node_2, 1000, 450, elapseTime, currTime, Animate::TRANS_TIME)
+        std::bind(&Animate::transform<GraphicNode>, node, 1200, 300, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&Animate::transform<GraphicNode>, node_2, 1000, 450, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform<GraphicNode>, node, 340, 321, elapseTime, currTime, Animate::TRANS_TIME)
+        std::bind(&Animate::transform<GraphicNode>, node, 340, 321, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform<GraphicNode>, node, 0, 0, elapseTime, currTime, Animate::TRANS_TIME),
-        std::bind(&nodeTransform<GraphicNode>, node_2, Window::WIDTH-NODE_SIZE, Window::HEIGHT-NODE_SIZE, elapseTime, currTime, Animate::TRANS_TIME)
+        std::bind(&Animate::transform<GraphicNode>, node, 0, 0, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&Animate::transform<GraphicNode>, node_2, Window::WIDTH-NODE_SIZE, Window::HEIGHT-NODE_SIZE, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform<GraphicNode>, node, 450, 450, elapseTime, currTime, Animate::TRANS_TIME),
-        std::bind(&nodeTransform<GraphicNode>, node_2, 0, 0, elapseTime, currTime, Animate::TRANS_TIME)
+        std::bind(&Animate::transform<GraphicNode>, node, 450, 450, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&Animate::transform<GraphicNode>, node_2, 0, 0, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform<GraphicNode>, node, 600, 200, elapseTime, currTime, Animate::TRANS_TIME),
-        std::bind(&nodeTransform<GraphicNode>, node_2, 600, 200, elapseTime, currTime, Animate::TRANS_TIME)
+        std::bind(&Animate::transform<GraphicNode>, node, 600, 200, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&Animate::transform<GraphicNode>, node_2, 600, 200, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform<GraphicNode>, node, 200, 200, elapseTime, currTime, Animate::TRANS_TIME),
-        std::bind(&nodeTransform<GraphicNode>, node_2, 1000, 600, elapseTime, currTime, Animate::TRANS_TIME)
+        std::bind(&Animate::transform<GraphicNode>, node, 200, 200, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&Animate::transform<GraphicNode>, node_2, 1000, 600, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform<GraphicNode>, node, 500, 500, elapseTime, currTime, Animate::TRANS_TIME),
-        std::bind(&nodeTransform<GraphicNode>, node_2, 500, 500+NODE_SIZE, elapseTime, currTime, Animate::TRANS_TIME)
+        std::bind(&Animate::transform<GraphicNode>, node, 500, 500, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&Animate::transform<GraphicNode>, node_2, 500, 500+NODE_SIZE, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeFadeOut<GraphicNode>, node, elapseTime, currTime, Animate::FADEOUT_TIME)
+        std::bind(&Animate::fadeOut<GraphicNode>, node, elapseTime, currTime, Animate::FADEOUT_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeFadeIn<GraphicNode>, node, elapseTime, currTime, Animate::FADEIN_TIME)
+        std::bind(&Animate::fadeIn<GraphicNode>, node, elapseTime, currTime, Animate::FADEIN_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform<GraphicNode>, node, 300, 500, elapseTime, currTime, Animate::TRANS_TIME),
-        std::bind(&nodeTransform<GraphicNode>, node_2, 900, 700, elapseTime, currTime, Animate::TRANS_TIME)
+        std::bind(&Animate::transform<GraphicNode>, node, 300, 500, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&Animate::transform<GraphicNode>, node_2, 900, 700, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform<GraphicNode>, node, 1200, 300, elapseTime, currTime, Animate::TRANS_TIME),
-        std::bind(&nodeTransform<GraphicNode>, node_2, 1000, 450, elapseTime, currTime, Animate::TRANS_TIME)
+        std::bind(&Animate::transform<GraphicNode>, node, 1200, 300, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&Animate::transform<GraphicNode>, node_2, 1000, 450, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform<GraphicNode>, node, 340, 321, elapseTime, currTime, Animate::TRANS_TIME)
+        std::bind(&Animate::transform<GraphicNode>, node, 340, 321, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform<GraphicNode>, node, 0, 0, elapseTime, currTime, Animate::TRANS_TIME),
-        std::bind(&nodeTransform<GraphicNode>, node_2, Window::WIDTH-NODE_SIZE, Window::HEIGHT-NODE_SIZE, elapseTime, currTime, Animate::TRANS_TIME)
+        std::bind(&Animate::transform<GraphicNode>, node, 0, 0, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&Animate::transform<GraphicNode>, node_2, Window::WIDTH-NODE_SIZE, Window::HEIGHT-NODE_SIZE, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform<GraphicNode>, node, 450, 450, elapseTime, currTime, Animate::TRANS_TIME),
-        std::bind(&nodeTransform<GraphicNode>, node_2, 0, 0, elapseTime, currTime, Animate::TRANS_TIME)
+        std::bind(&Animate::transform<GraphicNode>, node, 450, 450, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&Animate::transform<GraphicNode>, node_2, 0, 0, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform<GraphicNode>, node, 600, 200, elapseTime, currTime, Animate::TRANS_TIME),
-        std::bind(&nodeTransform<GraphicNode>, node_2, 600, 200, elapseTime, currTime, Animate::TRANS_TIME)
+        std::bind(&Animate::transform<GraphicNode>, node, 600, 200, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&Animate::transform<GraphicNode>, node_2, 600, 200, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeTransform<GraphicNode>, node, 200, 200, elapseTime, currTime, Animate::TRANS_TIME),
-        std::bind(&nodeTransform<GraphicNode>, node_2, 1000, 600, elapseTime, currTime, Animate::TRANS_TIME)
+        std::bind(&Animate::transform<GraphicNode>, node, 200, 200, elapseTime, currTime, Animate::TRANS_TIME),
+        std::bind(&Animate::transform<GraphicNode>, node_2, 1000, 600, elapseTime, currTime, Animate::TRANS_TIME)
     });
     queueOfScenes.push({
-        std::bind(&nodeFadeOut<GraphicNode>, node, elapseTime, currTime, Animate::FADEOUT_TIME),
-        std::bind(&nodeFadeOut<GraphicNode>, node_2, elapseTime, currTime, Animate::FADEOUT_TIME)
+        std::bind(&Animate::fadeOut<GraphicNode>, node, elapseTime, currTime, Animate::FADEOUT_TIME),
+        std::bind(&Animate::fadeOut<GraphicNode>, node_2, elapseTime, currTime, Animate::FADEOUT_TIME)
     });
 
     while (!raylib::WindowShouldClose()) {
         *elapseTime = raylib::GetFrameTime();
         std::cerr << "Elapse time: " << *elapseTime << '\n';
-        runScenes(queueOfScenes, currTime, elapseTime);
+        Animate::runScenes(queueOfScenes, currTime, elapseTime);
         raylib::BeginDrawing();
             raylib::ClearBackground(raylib::WHITE);
             node->draw();
