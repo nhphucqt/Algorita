40cfacb93af0cc617d257f9b1ba6f6b72e480c5b
nhphucqt <hoangphuc.qti@gmail.com>
2023-03-21 22:40:14 +0700

    Add + - operators

--- libs/core/ccppclass.cpp
+++ libs/core/ccppclass.cpp
@@ -5,3 +5,19 @@ TrVector::TrVector() {}
 TrVector::TrVector(float* _x, float* _y) {
     x = _x; y = _y;
 }
+
+raylib::Vector2 TrVector::tv2() const {
+    return raylib::Vector2{*x, *y};
+}
+
+raylib::Vector2 TrVector::operator - (const TrVector &v) const {
+    return tv2() - v.tv2();
+}
+
+raylib::Vector2 TrVector::operator - (const raylib::Vector2 &v) const {
+    return tv2() - v;
+}
+
+raylib::Vector2 TrVector::operator + (const raylib::Vector2 &v) const {
+    return tv2() + v;
+}
--- libs/core/ccppclass.h
+++ libs/core/ccppclass.h
@@ -1,12 +1,21 @@
 #ifndef CCPPCLASS_H
 #define CCPPCLASS_H
 
+// DO NOT INCLUDE ANYTHING EXCEPT C++ lib and raylib.h and rcore
+
+#include "../conf_raylib.h"
+#include "../rcore/rvector2.h"
+
 class TrVector { // Tracking vector | DO NOT USE 'new' operation, JUST FOR TRACKING
 public:
     float* x;
     float* y;
     TrVector();
     TrVector(float* _x, float* _y);
+    raylib::Vector2 tv2() const;
+    raylib::Vector2 operator - (const TrVector &v) const;
+    raylib::Vector2 operator - (const raylib::Vector2 &v) const;
+    raylib::Vector2 operator + (const raylib::Vector2 &v) const;
 };
 
 #endif
