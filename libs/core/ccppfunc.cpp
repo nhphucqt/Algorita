e82a92b508259c6b1d876c24409f7c52972d5ad1
nhphucqt <hoangphuc.qti@gmail.com>
2023-03-22 21:57:28 +0700

    Make outerShape return a Vector2, add outerShape for DoublyNode

--- libs/core/ccppfunc.cpp
+++ libs/core/ccppfunc.cpp
@@ -14,15 +14,15 @@ std::string cf::num2str(int x) {
     }
 }
 
-double cf::outerNull(const raylib::Vector2 &v) {
-    return 0;
+raylib::Vector2 cf::outerNull(const raylib::Vector2 &v) {
+    return Z_VECT;
 }
 
-double cf::outerCirc(float *size, const raylib::Vector2 &v) {
-    return *size / 2;
+raylib::Vector2 cf::outerCirc(float *size, const raylib::Vector2 &v) {
+    return resizeVector(v, *size/2);
 }
 
-double cf::outerSqur(float *size, const raylib::Vector2 &v) {
+raylib::Vector2 cf::outerSqur(float *size, const raylib::Vector2 &v) {
     double a = angle(X_AXIS, v);
     while (a > Geo::RAD90) {
         a -= Geo::RAD90;
@@ -30,7 +30,63 @@ double cf::outerSqur(float *size, const raylib::Vector2 &v) {
     if (a > Geo::RAD45) {
         a = Geo::RAD90 - a;
     }
-    // std::cerr << " +++ " << a * 180 / Geo::PI << '\n';
-    return *size / 2 / cos(a);
+    return resizeVector(v, *size / 2 / cos(a));
 }
 
+raylib::Vector2 cf::outerCircLeft(float *size, const raylib::Vector2 &v) {
+    double d = *size/6;
+    return resizeVector(normVector(v), d) + resizeVector(v, sqrt(sqr(*size/2) - sqr(d)));
+}
+
+raylib::Vector2 cf::outerCircRight(float *size, const raylib::Vector2 &v) {
+    double d = *size/6;
+    return resizeVector(revVector(normVector(v)), d) + resizeVector(v, sqrt(sqr(*size/2) - sqr(d)));
+}
+
+raylib::Vector2 cf::outerSqurLeft(float *size, const raylib::Vector2 &v) {
+    double d = *size/6;
+    double a = angle2(X_AXIS, v);
+    while (a < 0) {
+        a += Geo::RAD90;
+    }
+    while (a > Geo::RAD90) {
+        a -= Geo::RAD90;
+    }
+    double leng = 0;
+    double cross = (double)(*size) / sqrt(2);
+    double alpha = fabs(Geo::RAD45 - a);
+    double pivot = cross * sin(alpha);
+    if (a <= Geo::RAD45) {
+        double rem = fabs(pivot - d);
+        double beta = d >= pivot ? Geo::RAD45 - alpha : Geo::RAD45 + alpha;
+        leng = cross * cos(alpha) - rem / tan(beta);
+    } else {
+        double beta = Geo::RAD45 + alpha;
+        leng = cross * cos(alpha) - (pivot + d) / tan(beta);
+    }
+    return resizeVector(normVector(v), d) + resizeVector(v, leng);
+}
+
+raylib::Vector2 cf::outerSqurRight(float *size, const raylib::Vector2 &v) {
+    double d = *size/6;
+    double a = angle2(X_AXIS, v);
+    while (a > 0) {
+        a -= Geo::RAD90;
+    }
+    while (a < -Geo::RAD90) {
+        a += Geo::RAD90;
+    }
+    double leng = 0;
+    double cross = (double)(*size) / sqrt(2);
+    double alpha = fabs(-Geo::RAD45 - a);
+    double pivot = cross * sin(alpha);
+    if (a >= -Geo::RAD45) {
+        double rem = fabs(pivot - d);
+        double beta = d >= pivot ? Geo::RAD45 - alpha : Geo::RAD45 + alpha;
+        leng = cross * cos(alpha) - rem / tan(beta);
+    } else {
+        double beta = Geo::RAD45 + alpha;
+        leng = cross * cos(alpha) - (pivot + d) / tan(beta);
+    }
+    return resizeVector(revVector(normVector(v)), d) + resizeVector(v, leng);
+}
--- libs/core/ccppfunc.h
+++ libs/core/ccppfunc.h
@@ -10,10 +10,19 @@
 #include "../rcore/rvector2.h" 
 
 namespace cf {
+    template<typename T>
+    T sqr(T x) {
+        return x * x;
+    }
     std::string num2str(int x);
-    double outerNull(const raylib::Vector2 &v);
-    double outerCirc(float *size, const raylib::Vector2 &v);
-    double outerSqur(float *size, const raylib::Vector2 &v);
+    raylib::Vector2 outerNull(const raylib::Vector2 &v);
+    raylib::Vector2 outerCirc(float *size, const raylib::Vector2 &v);
+    raylib::Vector2 outerSqur(float *size, const raylib::Vector2 &v);
+
+    raylib::Vector2 outerCircLeft(float *size, const raylib::Vector2 &v);
+    raylib::Vector2 outerCircRight(float *size, const raylib::Vector2 &v);
+    raylib::Vector2 outerSqurLeft(float *size, const raylib::Vector2 &v);
+    raylib::Vector2 outerSqurRight(float *size, const raylib::Vector2 &v);
 }
 
 #endif
