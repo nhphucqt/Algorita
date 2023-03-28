ecc0dbd3a70a836bea23da6bbd88f0f8e78e059d
nhphucqt <hoangphuc.qti@gmail.com>
2023-03-28 09:55:56 +0700

    Make build command be more compatible

--- build.ps1
+++ build.ps1
@@ -9,5 +9,22 @@ if ($args.count -eq 1) {
 $file_path = $file_path.replace("\", "/")
 $project_name = $project_name.replace("\", "/")
 
+$compile_command = "C:/msys64/mingw64/bin/mingw32-make.exe"
+
+$all_cpp = (
+    "libs/rcore/*.cpp",
+    "libs/core/*.cpp",
+    "libs/graphic/*.cpp"
+)
+
+$compile_args = (
+    "RAYLIB_PATH=C:/raylib/raylib",
+    "PROJECT_NAME=${project_name}",
+    "OBJS=`"${file_path} $($all_cpp -join " ")`"",
+    "BUILD_MODE=DEBUG"
+)
+
 # C:\raylib\w64devkit\bin\windres.exe minecreeper.rc -o minecreeper.rc.data &&^
-C:\msys64\mingw64\bin\mingw32-make.exe RAYLIB_PATH=C:/raylib/raylib PROJECT_NAME=${project_name} OBJS="${file_path} libs/rcore/*.cpp libs/core/*.cpp libs/graphic/*.cpp" BUILD_MODE=DEBUG 
+
+# ($compile_command + " " + ($compile_args -join " "))
+Invoke-Expression ($compile_command + " " + ($compile_args -join " "))
