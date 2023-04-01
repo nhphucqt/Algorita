52ced811f76e94f574a7dc21a2682d9e15989682
nhphucqt <hoangphuc.qti@gmail.com>
2023-04-01 09:33:44 +0700

    Updated

--- main.cpp
+++ main.cpp
@@ -8,62 +8,51 @@
 
 #include "libs/global.h"
 
-#include "libs/graphic/GraphicNode.h"
-#include "libs/graphic/GraphicSinglyNode.h"
-#include "libs/graphic/GraphicDoublyNode.h"
-#include "libs/graphic/GraphicStack.h"
-#include "libs/graphic/GraphicSinglyLinkedList.h"
-
+#include "libs/graphic/objects/GraphicNode.h"
+#include "libs/graphic/objects/GraphicSinglyNode.h"
+#include "libs/graphic/objects/GraphicDoublyNode.h"
+#include "libs/graphic/objects/GraphicStack.h"
+#include "libs/graphic/objects/GraphicSinglyLinkedList.h"
+#include "libs/graphic/gui/GraphicButton.h"
 #include "libs/graphic/animation.h"
+#include "libs/graphic/screens/globalLayout.h"
+
+#include "libs/graphic/screens/ScreenSinglyLinkedList.h"
 
 int main() {
-    raylib::SetConfigFlags(raylib::FLAG_MSAA_4X_HINT);
-    raylib::SetConfigFlags(raylib::FLAG_WINDOW_ALWAYS_RUN);
+    SetRandomSeed(time(nullptr));
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
+    SetConfigFlags(FLAG_WINDOW_ALWAYS_RUN);
 
-    raylib::InitWindow(Window::WIDTH, Window::HEIGHT, "demo");
-    raylib::SetTargetFPS(Window::FPS);
+    InitWindow(Window::WIDTH, Window::HEIGHT, "demo");
+    SetTargetFPS(Window::FPS);
 
     // Load
-    Font::loadFont();
 
-    GraphicSinglyLinkedList li;
+    GuiLoadStyle("assets/styles/custom_style.rgs");
 
-    li.pushFront(1);
-    li.pushFront(2);
-    li.pushFront(3);
-    li.pushFront(4);
-    li.pushFront(5);
-    li.pushBack(6);
-    // li.pushBack(7);
-    // li.pushBack(8);
-    // li.pushBack(9);
-    li.pushFront(10);
-    li.pushAtKth(3, 11);
-    li.popFront();
-    li.popFront();
-    li.popBack();
-    li.popBack();
-    li.popAtKth(1);
+    Gfont::loadFont();
+    Layout::initialize();
 
-    // while (st.pop());
+    Screen::ScreenSinglyLinkedList screenSLL;
 
-    while (!raylib::WindowShouldClose()) {
-        Animate::elapseTime = raylib::GetFrameTime();
-        // std::cerr << "Elapse time: " << Animate::elapseTime << '\n';
+    while (!WindowShouldClose()) {
+        Animate::elapseTime = GetFrameTime(); // DO NOT DELETE
         Animate::queueOfScenes.run();
-        raylib::BeginDrawing();
-            raylib::ClearBackground(raylib::WHITE);
-            li.draw();
-        raylib::EndDrawing();
+        BeginDrawing();
+            ClearBackground(WHITE);
+            Layout::drawTopNavigation();
+            screenSLL.draw();
+        EndDrawing();
     }
 
-    raylib::CloseWindow();
+    CloseWindow();
 
 
     // Unload
-    Font::unloadFont();
+    Gfont::unloadFont();
 
-    li.destroy();
+    screenSLL.destroy();
 
     return 0;
 }
