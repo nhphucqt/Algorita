d424ca32346b9d61eafaa5b279fb528461fb83b0
nhphucqt <hoangphuc.qti@gmail.com>
2023-04-01 09:31:36 +0700

    Remove raylib namespace (raylib::)

--- libs/graphic/objects/StyledText.cpp
+++ libs/graphic/objects/StyledText.cpp
@@ -2,22 +2,22 @@
 
 StyledText::StyledText() {}
 
-StyledText::StyledText(const std::string &_content, raylib::Font* _font) {
+StyledText::StyledText(const std::string &_content, Font* _font) {
     content = _content;
     font = _font;
-    dim = raylib::MeasureTextEx(*font, content.c_str(), font->baseSize,  0);
+    dim = MeasureTextEx(*font, content.c_str(), font->baseSize,  0);
 }
 
-StyledText::StyledText(int _num, raylib::Font* _font) {
+StyledText::StyledText(int _num, Font* _font) {
     content = cf::num2str(_num);
     font = _font;
-    dim = raylib::MeasureTextEx(*font, content.c_str(), font->baseSize,  0);
+    dim = MeasureTextEx(*font, content.c_str(), font->baseSize,  0);
 }
 
-void StyledText::draw(const raylib::Vector2 &p, const raylib::Color &color) {
-    raylib::DrawTextEx(*font, content.c_str(), p, font->baseSize, 0, color);
+void StyledText::draw(const Vector2 &p, const Color &color) {
+    DrawTextEx(*font, content.c_str(), p, font->baseSize, 0, color);
 }
 
-void StyledText::draw(int x, int y, const raylib::Color &color) {
+void StyledText::draw(int x, int y, const Color &color) {
     draw(toVector2(x,y), color);
 }
--- libs/graphic/objects/StyledText.h
+++ libs/graphic/objects/StyledText.h
@@ -6,20 +6,20 @@
 
 #include "../../conf_raylib.h"
 #include "../../core/ccppfunc.h"
-#include "../../rcore/rvector2.h"
+#include "../../rcore/rVector2.h"
 
 class StyledText {
 public:
     std::string content;
-    raylib::Font* font;
-    raylib::Vector2 dim;
+    Font* font;
+    Vector2 dim;
 
     StyledText();
-    StyledText(const std::string &_content, raylib::Font* _font);
-    StyledText(int _num, raylib::Font* _font);
+    StyledText(const std::string &_content, Font* _font);
+    StyledText(int _num, Font* _font);
 
-    void draw(const raylib::Vector2 &p, const raylib::Color &color = raylib::BLACK);
-    void draw(int x, int y, const raylib::Color &color = raylib::BLACK);
+    void draw(const Vector2 &p, const Color &color = BLACK);
+    void draw(int x, int y, const Color &color = BLACK);
 };
 
 #endif
