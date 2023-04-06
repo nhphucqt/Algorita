dc38d601f9d86a7ff5c68f2bde81800b3b46f281
nhphucqt <hoangphuc.qti@gmail.com>
2023-04-06 23:51:30 +0700

    Comment all

--- libs/graphic/objects/GraphicStack.h
+++ libs/graphic/objects/GraphicStack.h
@@ -1,39 +1,39 @@
-#ifndef GRAPHIC_STACK_H
-#define GRAPHIC_STACK_H
+// #ifndef GRAPHIC_STACK_H
+// #define GRAPHIC_STACK_H
 
-#include <assert.h>
-#include <iostream>
-#include <functional>
+// #include <assert.h>
+// #include <iostream>
+// #include <functional>
 
-#include "../../global.h"
-#include "../../core/ccppfunc.h"
-#include "GraphicSinglyNode.h"
-#include "../../animation/animation.h"
+// #include "../../global.h"
+// #include "../../core/ccppfunc.h"
+// #include "GraphicSinglyNode.h"
+// #include "../../animation/animation.h"
 
-#include "../../core/objects/CoreStack.h"
+// #include "../../core/objects/CoreStack.h"
 
-class GraphicStack {
-private:
-    GraphicSinglyNode* pHead;
-    CoreStack core;
+// class GraphicStack {
+// private:
+//     GraphicSinglyNode* pHead;
+//     CoreStack core;
     
-    bool animatePush(int val); // ALWAYS PUSH TO NEW SCENE
+//     bool animatePush(int val); // ALWAYS PUSH TO NEW SCENE
 
-    bool animatePop_1(); // ALWAYS PUSH TO NEW SCENE
-    bool animatePop_2(); // ALWAYS PUSH TO NEW SCENE
+//     bool animatePop_1(); // ALWAYS PUSH TO NEW SCENE
+//     bool animatePop_2(); // ALWAYS PUSH TO NEW SCENE
 
-    bool transformAllNodes(int Tx, int Ty);
+//     bool transformAllNodes(int Tx, int Ty);
 
-public:
+// public:
 
-    GraphicStack();
+//     GraphicStack();
 
-    void push(int val);
-    bool pop();
+//     void push(int val);
+//     bool pop();
 
-    void draw();
+//     void draw();
 
-    void destroy();
-};
+//     void destroy();
+// };
 
-#endif
+// #endif
