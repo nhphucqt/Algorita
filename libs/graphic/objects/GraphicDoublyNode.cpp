2db92ca9ada08084d71db2d7aea65fb673b5ebab
nhphucqt <hoangphuc.qti@gmail.com>
2023-04-18 20:36:32 +0700

    Updated and Added features

--- libs/graphic/objects/GraphicDoublyNode.cpp
+++ libs/graphic/objects/GraphicDoublyNode.cpp
@@ -22,15 +22,74 @@ GraphicDoublyNode::GraphicDoublyNode(float _x, float _y, float _s, bool _sqr) : 
 void GraphicDoublyNode::setNext(GraphicDoublyNode* pNode) {
     pNext = pNode;
     aNext = GraphicTrackArrow(&x, &y, &pNode->x, &pNode->y, &size, &pNode->size, outerShapeOut, pNode->outerShapeIn);
-    pNode->pPrev = this;
-    pNode->aPrev = GraphicTrackArrow(&pNode->x, &pNode->y, &x, &y, &pNode->size, &size, pNode->outerShapeOut, outerShapeIn);
 }
 
 void GraphicDoublyNode::setPrev(GraphicDoublyNode* pNode) {
     pPrev = pNode;
-    aPrev = GraphicTrackArrow(&x, &y, &pNode->x, &pNode->y, &size, &pNode->size, outerShapeOut, pNode->outerShapeIn);
-    pNode->pNext = this;
-    pNode->aNext = GraphicTrackArrow(&pNode->x, &pNode->y, &x, &y, &pNode->size, &size, pNode->outerShapeOut, outerShapeIn);
+    aPrev = GraphicTrackArrow(&x, &y, &pNode->x, &pNode->y, &size, &pNode->size, outerShapeOut, pNode->outerShapeIn); 
+}
+
+void GraphicDoublyNode::setRevPrev(GraphicDoublyNode* pNode) {
+    pPrev = pNode;
+    aPrev = GraphicTrackArrow(&pNode->x, &pNode->y, &x, &y, &pNode->size, &size, pNode->outerShapeOut, outerShapeIn); 
+}
+
+void GraphicDoublyNode::updateNext(GraphicDoublyNode* pNode) {
+    pNext = pNode;
+    aNext.pB = TrVector(&pNode->x, &pNode->y);
+    aNext.sB = &pNode->size;
+    aNext.ftB = pNode->outerShapeIn;
+    aNext.transA = Z_VECT;
+    aNext.transB = Z_VECT;
+}
+
+void GraphicDoublyNode::updatePrev(GraphicDoublyNode* pNode) {
+    pPrev = pNode;
+    aPrev.pB = TrVector(&pNode->x, &pNode->y);
+    aPrev.sB = &pNode->size;
+    aPrev.ftB = pNode->outerShapeIn;
+    aPrev.transA = Z_VECT;
+    aPrev.transB = Z_VECT;
+}
+
+
+
+void GraphicDoublyNode::setArrowHeadTrans(GraphicTrackArrow* arrow, GraphicDoublyNode* node) {
+    Vector2 cA = arrow->pA + toSqrVector2(*arrow->sA / 2);
+    Vector2 cB = arrow->pB + toSqrVector2(*arrow->sB / 2);
+    Vector2 cC = toVector2(node->x, node->y) + toSqrVector2(node->size / 2);
+
+    Vector2 AB = cB - cA;
+    Vector2 BA = cA - cB;
+    Vector2 CA = cA - cC;
+    Vector2 AC = cC - cA;
+
+    Vector2 newB = cB + arrow->ftB(BA);
+    Vector2 newC = cC + node->outerShapeIn(CA);
+
+    Vector2 BC = newC - newB;
+
+    arrow->transA = outerShapeOut(AC) - outerShapeOut(AB);
+    arrow->transB = BC;
+}
+
+void GraphicDoublyNode::setArrowTailTrans(GraphicTrackArrow* arrow, GraphicDoublyNode* node) {
+    Vector2 cA = arrow->pA + toSqrVector2(*arrow->sA / 2);
+    Vector2 cB = arrow->pB + toSqrVector2(*arrow->sB / 2);
+    Vector2 cC = toVector2(node->x, node->y) + toSqrVector2(node->size / 2);
+
+    Vector2 AB = cB - cA;
+    Vector2 BA = cA - cB;
+    Vector2 CB = cB - cC;
+    Vector2 BC = cC - cB;
+
+    Vector2 newA = cA + arrow->ftA(AB);
+    Vector2 newC = cC + node->outerShapeOut(CB);
+
+    Vector2 AC = newC - newA;
+
+    arrow->transA = AC;
+    arrow->transB = arrow->ftB(BC) - arrow->ftB(BA);
 }
 
 void GraphicDoublyNode::draw() {
--- libs/graphic/objects/GraphicDoublyNode.h
+++ libs/graphic/objects/GraphicDoublyNode.h
@@ -16,6 +16,16 @@ public:
     void setNext(GraphicDoublyNode* pNode);
     void setPrev(GraphicDoublyNode* pNode);
 
+    void setRevPrev(GraphicDoublyNode* pNode);
+
+    void updateNext(GraphicDoublyNode* pNode);
+    void updatePrev(GraphicDoublyNode* pNode);
+
+
+    void setArrowHeadTrans(GraphicTrackArrow* arrow, GraphicDoublyNode* node);
+    void setArrowTailTrans(GraphicTrackArrow* arrow, GraphicDoublyNode* node);
+
+
     void draw();
 };
 
