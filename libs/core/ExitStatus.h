7d647178b2022f84a80ecd6ca7b6133f73c89466
nhphucqt <hoangphuc.qti@gmail.com>
2023-04-18 20:33:57 +0700

    Add ExitMess

--- libs/core/ExitStatus.h
+++ libs/core/ExitStatus.h
@@ -3,6 +3,9 @@
 
 #include <string>
 
+#include "../global.h"
+#include "ccppfunc.h"
+
 class ExitStatus {
 public:
     bool success;
@@ -11,5 +14,16 @@ public:
     ExitStatus();
     ExitStatus(bool _success, const std::string &_message);
 };
+
+namespace ExitMess {
+    const ExitStatus SUCCESS(true, "");
+    const ExitStatus FAIL(false, "");
+
+    const ExitStatus FAIL_VALUE_OOB(false, "Value is out of bounds: Allow from " + cf::num2str(Core::NODE_MIN_VALUE) + " to " + cf::num2str(Core::NODE_MAX_VALUE));
+
+    const ExitStatus FAIL_LL_EMPTY(false, "The list is empty");
+    const ExitStatus FAIL_LL_SIZE_OOB(false, "Size is out of bounds: Allow from 0 to " + cf::num2str(Core::MAX_NUM_NODE_SLL));
+    const ExitStatus FAIL_LL_REACH_MAX_SIZE(false, "The list has reached maximum size which is " + cf::num2str(Core::MAX_NUM_NODE_SLL));
+}
 
 #endif
