c26c9a4a5e5be9f1fad145aaf9becbfe9dc26df2
nhphucqt <hoangphuc.qti@gmail.com>
2023-04-19 19:48:29 +0700

    Created Queue Screen

---
+++ libs/graphic/screens/ScreenQueue.cpp
@@ -0,0 +1,127 @@
+#include "ScreenQueue.h"
+
+Screen::ScreenQueue::ScreenQueue() {}
+
+void Screen::ScreenQueue::load() { // Ensure that obj has to be destroy before (prevent MEMORY LEAKING)
+    inputUpdate.init(Rectangle{(Gui::BUTTON_OPER_WIDTH + Gui::BUTTON_OPER_DIST_X) * 1 + Gui::LABEL_PADDING_LEFT + Gui::BUTTON_OPER_GO_WIDTH, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 4 + 5, Gui::BUTTON_OPER_WIDTH - Gui::BUTTON_OPER_GO_WIDTH * 3 - 20, Gui::INPUT_BOX_HEIGHT}, Graphic::MAX_DIGIT_INPUT, true, false);
+    inputPush.init(Rectangle{(Gui::BUTTON_OPER_WIDTH + Gui::BUTTON_OPER_DIST_X) * 1 + Gui::LABEL_PADDING_LEFT + Gui::BUTTON_OPER_GO_WIDTH, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 3 + 5, Gui::BUTTON_OPER_WIDTH - Gui::BUTTON_OPER_GO_WIDTH * 3 - 20, Gui::INPUT_BOX_HEIGHT}, Graphic::MAX_DIGIT_INPUT, true,  false);
+    inputUserDefined.init(Rectangle{(Gui::BUTTON_OPER_WIDTH + Gui::BUTTON_OPER_DIST_X) * 3 + 5, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 5 + 5, Gui::USER_DEF_BUTTON_WIDTH - Gui::BUTTON_OPER_GO_WIDTH - 15, Gui::USER_DEF_INPUT_HEIGHT}, Graphic::MAX_SIZE_INPUT, false, false);
+
+    toggleCreateType.init(Rectangle{0, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 6, Gui::BUTTON_OPER_WIDTH, Gui::BUTTON_OPER_HEIGHT}, "Create", false);
+    toggleUpdateType.init(Rectangle{0, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 4, Gui::BUTTON_OPER_WIDTH, Gui::BUTTON_OPER_HEIGHT}, "Update", false);
+    togglePushType.init(Rectangle{0, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 3, Gui::BUTTON_OPER_WIDTH, Gui::BUTTON_OPER_HEIGHT}, "Push", false);
+
+    toggleUserDefine.init(Rectangle{(Gui::BUTTON_OPER_WIDTH + Gui::BUTTON_OPER_DIST_X) * 3, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 6, Gui::USER_DEF_BUTTON_WIDTH, Gui::USER_DEF_BUTTON_HEIGHT}, "Input", false);
+
+    exitMessage = StyledText(std::string(), Gfont::defaultFont);
+
+    fileDialogState = InitGuiFileDialog(Gui::FILE_DIALOG_WIDTH, Gui::FILE_DIALOG_HEIGHT, GetWorkingDirectory(), false);
+}
+
+void Screen::ScreenQueue::init() {
+    currOperationType = OTNULL;
+    currOperation = ONULL;
+    exitMessage.assign(obj.initialize(GetRandomValue(1, Core::MAX_NUM_NODE_SLL), &ALOG).message);
+}
+
+void Screen::ScreenQueue::draw() {
+    bool keyActive = true;
+    if (fileDialogState.windowActive) {
+        GuiLock();
+        keyActive = false;
+    }
+    if (toggleCreateType.draw()) {
+        if (toggleCreateType.justToggle()) {
+            currOperationType = CREATE;
+        } else if (currOperationType != CREATE) {
+            toggleCreateType.deactive();
+        }
+        if (GuiButton(Rectangle{(Gui::BUTTON_OPER_WIDTH + Gui::BUTTON_OPER_DIST_X) * 1, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 6, Gui::BUTTON_OPER_WIDTH, Gui::BUTTON_OPER_HEIGHT}, "Empty")) {
+            exitMessage.assign(obj.initialize(0, &ALOG).message);
+        }
+        if (GuiButton(Rectangle{(Gui::BUTTON_OPER_WIDTH + Gui::BUTTON_OPER_DIST_X) * 2, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 6, Gui::BUTTON_OPER_WIDTH, Gui::BUTTON_OPER_HEIGHT}, "Random")) {
+            exitMessage.assign(obj.initialize(GetRandomValue(1, Core::MAX_NUM_NODE_SLL), &ALOG).message);
+        }
+        if (toggleUserDefine.draw()) {
+            GuiDummyRec(Rectangle{(Gui::BUTTON_OPER_WIDTH + Gui::BUTTON_OPER_DIST_X) * 3, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 5, Gui::USER_DEF_BUTTON_WIDTH, Gui::USER_DEF_BUTTON_HEIGHT}, "");
+            keyActive &= !inputUserDefined.draw();
+            if (GuiButton(Rectangle{(Gui::BUTTON_OPER_WIDTH + Gui::BUTTON_OPER_DIST_X) * 3 + Gui::USER_DEF_BUTTON_WIDTH - Gui::BUTTON_OPER_GO_WIDTH - 5, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 5 + 5, Gui::BUTTON_OPER_GO_HEIGHT, Gui::BUTTON_OPER_GO_HEIGHT}, "Go") || (keyActive && IsKeyPressed(KEY_ENTER))) {
+                exitMessage.assign(obj.initialize(inputUserDefined.getStr(), &ALOG).message);
+            }
+        }
+        if (GuiButton(Rectangle{(Gui::BUTTON_OPER_WIDTH + Gui::BUTTON_OPER_DIST_X) * 3 + Gui::USER_DEF_BUTTON_WIDTH + Gui::BUTTON_OPER_DIST_X, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 6, Gui::FILE_DIALOG_OPEN_BUTTON_WIDTH, Gui::FILE_DIALOG_OPEN_BUTTON_HEIGHT}, "File")) {
+            fileDialogState.windowActive = true;
+        }
+    }
+    // Peek type Button
+    if (GuiButton(Rectangle{0, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 5, Gui::BUTTON_OPER_WIDTH, Gui::BUTTON_OPER_HEIGHT}, "Peek")) {
+        currOperationType = PEEK;
+        exitMessage.assign(obj.peek(&ALOG).message);
+    }
+    // Update type Button
+    if (toggleUpdateType.draw()) {
+        if (toggleUpdateType.justToggle()) {
+            currOperationType = UPDATE;
+        } else if (currOperationType != UPDATE) {
+            toggleUpdateType.deactive();
+        }
+        GuiDummyRec(Rectangle{(Gui::BUTTON_OPER_WIDTH + Gui::BUTTON_OPER_DIST_X) * 1, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 4, Gui::BUTTON_OPER_WIDTH, Gui::BUTTON_OPER_HEIGHT}, nullptr);
+        GuiLabel(Rectangle{(Gui::BUTTON_OPER_WIDTH + Gui::BUTTON_OPER_DIST_X) * 1 + Gui::LABEL_PADDING_LEFT, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 4, Gui::BUTTON_OPER_GO_WIDTH, Gui::BUTTON_OPER_HEIGHT}, "v =");
+        keyActive &= !inputUpdate.draw();
+        if (GuiButton(Rectangle{(Gui::BUTTON_OPER_WIDTH + Gui::BUTTON_OPER_DIST_X) * 1 + Gui::BUTTON_OPER_WIDTH - (Gui::BUTTON_OPER_GO_WIDTH + 5) * 2, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 4 + 5, Gui::BUTTON_OPER_GO_HEIGHT, Gui::BUTTON_OPER_GO_HEIGHT}, Gicon::BUTTON_RANDOM)) {
+            inputUpdate.setnum(GetRandomValue(Core::NODE_MIN_VALUE, Core::NODE_MAX_VALUE));
+        }
+        if (GuiButton(Rectangle{(Gui::BUTTON_OPER_WIDTH + Gui::BUTTON_OPER_DIST_X) * 1 + Gui::BUTTON_OPER_WIDTH - (Gui::BUTTON_OPER_GO_WIDTH + 5) * 1, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 4 + 5, Gui::BUTTON_OPER_GO_HEIGHT, Gui::BUTTON_OPER_GO_HEIGHT}, "Go") || (keyActive && IsKeyPressed(KEY_ENTER))) {
+            exitMessage.assign(obj.update(inputUpdate.getNum(), &ALOG).message);
+        }
+    }
+    // Push type Button
+    if (togglePushType.draw()) {
+        if (togglePushType.justToggle()) {
+            currOperationType = PUSH;
+        } else if (currOperationType != PUSH) {
+            togglePushType.deactive();
+        }
+        GuiDummyRec(Rectangle{(Gui::BUTTON_OPER_WIDTH + Gui::BUTTON_OPER_DIST_X) * 1, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 3, Gui::BUTTON_OPER_WIDTH, Gui::BUTTON_OPER_HEIGHT}, nullptr);
+        GuiLabel(Rectangle{(Gui::BUTTON_OPER_WIDTH + Gui::BUTTON_OPER_DIST_X) * 1 + Gui::LABEL_PADDING_LEFT, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 3, Gui::BUTTON_OPER_GO_WIDTH, Gui::BUTTON_OPER_HEIGHT}, "v =");
+        keyActive &= !inputPush.draw();
+        if (GuiButton(Rectangle{(Gui::BUTTON_OPER_WIDTH + Gui::BUTTON_OPER_DIST_X) * 1 + Gui::BUTTON_OPER_WIDTH - (Gui::BUTTON_OPER_GO_WIDTH + 5) * 2, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 3 + 5, Gui::BUTTON_OPER_GO_HEIGHT, Gui::BUTTON_OPER_GO_HEIGHT}, Gicon::BUTTON_RANDOM)) {
+            inputPush.setnum(GetRandomValue(Core::NODE_MIN_VALUE, Core::NODE_MAX_VALUE));
+        }
+        if (GuiButton(Rectangle{(Gui::BUTTON_OPER_WIDTH + Gui::BUTTON_OPER_DIST_X) * 1 + Gui::BUTTON_OPER_WIDTH - (Gui::BUTTON_OPER_GO_WIDTH + 5) * 1, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 3 + 5, Gui::BUTTON_OPER_GO_HEIGHT, Gui::BUTTON_OPER_GO_HEIGHT}, "Go") || (keyActive && IsKeyPressed(KEY_ENTER))) {
+            exitMessage.assign(obj.push(inputPush.getNum(), &ALOG).message);
+        }
+    }
+    // Pop type Button
+    if (GuiButton(Rectangle{0, Window::HEIGHT - Layout::BOTTOM_HEIGHT - Gui::BUTTON_OPER_HEIGHT * 2, Gui::BUTTON_OPER_WIDTH, Gui::BUTTON_OPER_HEIGHT}, "Pop")) {
+        currOperationType = POP;
+        exitMessage.assign(obj.pop(&ALOG).message);
+    }
+
+    if (fileDialogState.SelectFilePressed) {
+        // std::cerr << fileDialogState.fileNameText << '\n';
+        std::ifstream fin(fileDialogState.fileNameText);
+        std::string data;
+        std::getline(fin, data);
+        fin.close();
+        obj.initialize(data, &ALOG);
+        fileDialogState.SelectFilePressed = false;
+    }
+
+    // std::cerr << "SLLS:draw start ALOG.run()\n";
+    // while (!ALOG.run());
+    // std::cerr << "SLLS:draw end ALOG.run()\n";
+    ALOG.run();
+    obj.draw();
+    ALOG.draw(keyActive);
+    exitMessage.draw(10, 865, RED);
+    Layout::drawTopNavigation(keyActive);
+    
+    GuiUnlock();
+    GuiFileDialog(&fileDialogState);
+}
+
+void Screen::ScreenQueue::destroy() {
+    obj.destroy();
+    ALOG.destroy();
+}
---
+++ libs/graphic/screens/ScreenQueue.h
@@ -0,0 +1,59 @@
+#ifndef SCREEN_QUEUE_H
+#define SCREEN_QUEUE_H
+
+#include <string.h>
+
+#include "../../conf_raylib.h"
+
+#include "../objects/GraphicQueue.h"
+#include "../../animation/ListOfOperationsGroups.h"
+#include "../gui/Codeblock.h"
+#include "../../core/userfunc.h"
+
+#include "../../rcore/rguiTextBox.h"
+#include "../../rcore/rguiToggle.h"
+#include "../../rcore/rRectangle.h"
+
+#include "../../external/gui_file_dialog.h"
+
+#include "globalLayout.h"
+
+namespace Screen {
+    class ScreenQueue {
+    private:
+        enum OperationType {OTNULL, CREATE, PEEK, UPDATE, PUSH, POP} currOperationType;
+        enum Operation {ONULL, CREATE_USER_DEF} currOperation;
+
+        GraphicQueue obj;
+
+        GuiTextBoxState inputUpdate;
+        GuiTextBoxState inputPush;
+        GuiTextBoxState inputUserDefined;
+
+        GuiToggleState toggleCreateType;
+        GuiToggleState toggleUpdateType;
+        GuiToggleState toggleSearchType;
+        GuiToggleState togglePushType;
+        
+        GuiToggleState toggleUserDefine;
+
+
+        StyledText exitMessage;
+
+        ListOfOperationsGroups<GraphicQueue> ALOG;
+
+        GuiFileDialogState fileDialogState;
+
+    public:
+        ScreenQueue();
+
+        void load();
+        void init();
+        void draw();
+        void destroy();
+    };
+
+}
+
+
+#endif
