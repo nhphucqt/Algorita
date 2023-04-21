460d1dc33dcad2a84255e31e7fc1698a9f87d3f2
nhphucqt <hoangphuc.qti@gmail.com>
2023-04-21 13:58:10 +0700

    Updated

--- libs/graphic/screens/ScreenQueue.h
+++ libs/graphic/screens/ScreenQueue.h
@@ -22,7 +22,7 @@ namespace Screen {
     class ScreenQueue {
     private:
         enum OperationType {OTNULL, CREATE, PEEK, UPDATE, PUSH, POP} currOperationType;
-        enum Operation {ONULL, CREATE_USER_DEF} currOperation;
+        enum Operation {ONULL, CREATE_EMPTY, CREATE_RANDOM, CREATE_USER_DEF, CREATE_FILE} currOperation;
 
         GraphicQueue obj;
 
