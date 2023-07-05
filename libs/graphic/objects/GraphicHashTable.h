c7ce597bc551f4019bf1ac8e5f09fbee350df8da
nhphucqt <hoangphuc.qti@gmail.com>
2023-07-06 01:46:37 +0700

    Added chainSize in HashTable, Created Animation functions

--- libs/graphic/objects/GraphicHashTable.h
+++ libs/graphic/objects/GraphicHashTable.h
@@ -4,11 +4,14 @@
 #include <list>
 #include "GraphicSinglyNode.h"
 #include "../../core/ExitStatus.h"
+#include "../../core/validator.h"
+#include "../../core/userfunc.h"
 #include "../../animation/ListOfOperationsGroups.h"
 
 class GraphicHashTable {
 private:
     int _size;
+    int* chainSize;
     GraphicSinglyNode** pHead;
     std::list<GraphicSinglyNode*> nodes;
     std::list<GraphicTrackArrow*> arrows;
@@ -24,11 +27,12 @@ public:
 
     ExitStatus initialize(int initSize, int initNum, ListOfOperationsGroups* ALOG); // Randomly initialize
     ExitStatus initialize(int initSize, std::vector<int> vals, ListOfOperationsGroups* ALOG); // Initialize with given values
+    ExitStatus initialize(int initSize, std::string strVals, ListOfOperationsGroups* ALOG); // Initialize with given values
     ExitStatus initialize(std::string strVals, ListOfOperationsGroups* ALOG); // Initialize with given values
 
     ExitStatus searchFirst(int val, ListOfOperationsGroups* ALOG);
     ExitStatus push(int val, ListOfOperationsGroups* ALOG);
-    ExitStatus popFront(ListOfOperationsGroups* ALOG);
+    ExitStatus pop(int val, ListOfOperationsGroups* ALOG);
 
     void draw();
 
