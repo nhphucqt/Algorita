5ac3bf4f45009b637e59f40616f58f65920cd202
nhphucqt <hoangphuc.qti@gmail.com>
2023-08-09 00:40:58 +0700

    Added exit messages

--- libs/core/ExitStatus.h
+++ libs/core/ExitStatus.h
@@ -35,6 +35,8 @@ namespace ExitMess {
     const ExitStatus FAIL_HT_SIZE_OOB(false, "M is out of bounds: Allow from 1 to " + cf::num2str(Core::MAX_NUM_HT_HOR_ELM));
     const ExitStatus FAIL_HT_REACH_MAX_VER_SIZE(false, "The coresponding chain has reached maximum size which is " + cf::num2str(Core::MAX_NUM_HT_VER_ELM));
     const ExitStatus FAIL_HT_NUM_OOB(false, "N is out of bounds: Allow from 0 to Mx" + cf::num2str(Core::MAX_NUM_HT_VER_ELM));
+
+    const ExitStatus FAIL_AVL_REACH_MAX_SIZE(false, "The AVL Tree has reached maximum size which is " + cf::num2str(Core::MAX_NUM_AVL_ELM));
 }
 
 #endif
